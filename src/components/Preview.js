fb7d48354647723b837298a2abd38469f0be14a6
Nimira43 <n.rabone72@aol.com>
2024-11-02 14:16:24 +0000

    Setting styles for code and links for the preview section

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,7 +1,10 @@
 export default function Preview() {
   return (
     <div className="preview">
-      <h1 className="preview-heading">Preview</h1>
+      <h1 className="preview-heading">Markdown Preview</h1>
+      <code>npm install express</code>
+      <code>npm install react-router-dom</code>
+      <a href="https://bbc.com">BBC News</a>
     </div>
   )
 }
