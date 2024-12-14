794da27ce820e9e5e1105d97943ad5d84623d5dc
Nimira43 <n.rabone72@aol.com>
2024-12-14 03:50:25 +0000

    Created new project in repo - a crypto checker

--- src/components/Markdown.js
+++
@@ -1,9 +0,0 @@
-export default function Markdown() {
-  return (
-    <>
-      <div className="markdown">
-        <textarea name="markdown" id="markdown" placeholder="Enter Text" className="textarea"></textarea>
-      </div>
-    </>
-  )
-}
